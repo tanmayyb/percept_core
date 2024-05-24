3f663f1d7bf8eab4ae1e48d7fa629ac06a2e2622
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2024-05-24 19:53:23 -0400

    added no check certificate

--- peract/scripts/quickstart_download.sh
+++ peract/scripts/quickstart_download.sh
@@ -1,3 +1,3 @@
-wget https://github.com/peract/peract/releases/download/v1.0.0/peract_600k.zip
+wget https://github.com/peract/peract/releases/download/v1.0.0/peract_600k.zip --no-check-certificate
 unzip peract_600k.zip
 rm peract_600k.zip
