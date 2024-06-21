597ee669b3c0696d460a029b82e76dabbf145c44
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2024-06-20 22:18:59 -0400

    fix scripts

--- src/scripts/run_test.sh
+++ src/scripts/run_test.sh
@@ -7,5 +7,5 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$COPPELIASIM_ROOT
 export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT
 export SETUPTOOLS_USE_DISTUTILS=stdlib
 
-cd $PERCEPT_ROOT
-python scripts/test_scene.py
+cd $PERCEPT_SRC
+python tests/test_scene.py
---
+++ src/tests/test_rlbench.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+
+cd $PERCEPT_ROOT
+source peract_env/bin/activate
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$COPPELIASIM_ROOT
+export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT
+# export SETUPTOOLS_USE_DISTUTILS=stdlib
+
+cd $PERACT_ROOT
+CUDA_VISIBLE_DEVICES=0 python eval.py \
+    rlbench.tasks=[open_drawer] \
+    rlbench.task_name='multi' \
+    rlbench.demo_path=$PERACT_ROOT/data/val \
+    framework.gpu=0 \
+    framework.logdir=$PERACT_ROOT/ckpts/ \
+    framework.start_seed=0 \
+    framework.eval_envs=1 \
+    framework.eval_from_eps_number=0 \
+    framework.eval_episodes=10 \
+    framework.csv_logging=True \
+    framework.tensorboard_logging=True \
+    framework.eval_type='last' \
+    rlbench.headless=False
