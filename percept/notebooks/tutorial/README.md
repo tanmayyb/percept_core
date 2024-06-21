528e7460810191dc818109c408664e585866b4c0
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2024-06-20 21:21:19 -0400

    moved peract and pmaf repos

--- percept/notebooks/tutorial/README.md
+++
@@ -1,8 +0,0 @@
-# PerAct Annotated Tutorial
-
-
-Creds: Mohit Shridhar and others whose work Mohit built upon
-
-```
-./setup.bash
-```
--- percept/notebooks/tutorial/setup.bash
+++
@@ -1,4 +0,0 @@
-virtualenv -p $(which python3.8) --system-site-packages peract_env  
-source peract_env/bin/activate
-pip ipykernel jupyter matplotlib natsort
-pip install scipy ftfy regex tqdm torch git+https://github.com/openai/CLIP.git einops pyrender==0.1.45 trimesh==3.9.34 pycollada==0.6
