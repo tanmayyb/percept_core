916299e9ad7c8586482c62d6b8da02bbe71cdc87
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2024-08-07 23:15:40 +0530

    update datacap script

--- src/scripts/run_datacap.sh
+++ src/scripts/run_datacap.sh
@@ -7,4 +7,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$COPPELIASIM_ROOT
 export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT
 
 cd $PERCEPT_SRC
-python scripts/dataset/dualarms_2cam_datacap.py
+python scripts/dataset/dualarms_3cam_datacap.py
