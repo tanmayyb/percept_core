972bbef02792bbdf98c7b0afe26c78a2cd51e90f
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2024-09-04 01:01:38 +0530

    init sim stuff

---
+++ src/simulation/training/hello_world.py
@@ -0,0 +1,134 @@
+#@title All `dm_control` imports required for this tutorial
+
+# The basic mujoco wrapper.
+from dm_control import mujoco
+
+# Access to enums and MuJoCo library functions.
+from dm_control.mujoco.wrapper.mjbindings import enums
+from dm_control.mujoco.wrapper.mjbindings import mjlib
+
+# PyMJCF
+from dm_control import mjcf
+
+# Composer high level imports
+from dm_control import composer
+from dm_control.composer.observation import observable
+from dm_control.composer import variation
+
+# Imports for Composer tutorial example
+from dm_control.composer.variation import distributions
+from dm_control.composer.variation import noises
+from dm_control.locomotion.arenas import floors
+
+# Control Suite
+from dm_control import suite
+
+# Run through corridor example
+from dm_control.locomotion.walkers import cmu_humanoid
+from dm_control.locomotion.arenas import corridors as corridor_arenas
+from dm_control.locomotion.tasks import corridors as corridor_tasks
+
+# Soccer
+from dm_control.locomotion import soccer
+
+# Manipulation
+from dm_control import manipulation
+
+
+#@title Other imports and helper functions
+
+# General
+import copy
+import os
+import itertools
+from IPython.display import clear_output
+import numpy as np
+
+# Graphics-related
+import matplotlib
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+from IPython.display import HTML
+import PIL.Image
+# Internal loading of video libraries.
+
+# Use svg backend for figure rendering
+# %config InlineBackend.figure_format = 'svg'
+
+# Font sizes
+SMALL_SIZE = 8
+MEDIUM_SIZE = 10
+BIGGER_SIZE = 12
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+# Inline video helper function
+if os.environ.get('COLAB_NOTEBOOK_TEST', False):
+  # We skip video generation during tests, as it is quite expensive.
+  display_video = lambda *args, **kwargs: None
+else:
+  def display_video(frames, framerate=30):
+    height, width, _ = frames[0].shape
+    dpi = 70
+    orig_backend = matplotlib.get_backend()
+    matplotlib.use('Agg')  # Switch to headless 'Agg' to inhibit figure rendering.
+    fig, ax = plt.subplots(1, 1, figsize=(width / dpi, height / dpi), dpi=dpi)
+    matplotlib.use(orig_backend)  # Switch back to the original backend.
+    ax.set_axis_off()
+    ax.set_aspect('equal')
+    ax.set_position([0, 0, 1, 1])
+    im = ax.imshow(frames[0])
+    def update(frame):
+      im.set_data(frame)
+      return [im]
+    interval = 1000/framerate
+    anim = animation.FuncAnimation(fig=fig, func=update, frames=frames,
+                                   interval=interval, blit=True, repeat=False)
+    return HTML(anim.to_html5_video())
+
+# Seed numpy's global RNG so that cell outputs are deterministic. We also try to
+# use RandomState instances that are local to a single cell wherever possible.
+np.random.seed(42)
+
+
+walker = cmu_humanoid.CMUHumanoidPositionControlledV2020(
+    observable_options={'egocentric_camera': dict(enabled=True)})
+
+
+arena = corridor_arenas.WallsCorridor(
+    wall_gap=3.,
+    wall_width=distributions.Uniform(2., 3.),
+    wall_height=distributions.Uniform(2.5, 3.5),
+    corridor_width=4.,
+    corridor_length=30.,
+)
+
+task = corridor_tasks.RunThroughCorridor(
+    walker=walker,
+    arena=arena,
+    walker_spawn_position=(0.5, 0, 0),
+    target_velocity=3.0,
+    physics_timestep=0.005,
+    control_timestep=0.03,
+)
+
+
+env = composer.Environment(
+    task=task,
+    time_limit=10,
+    random_state=np.random.RandomState(42),
+    strip_singleton_obs_buffer_dim=True,
+)
+env.reset()
+pixels = []
+for camera_id in range(3):
+  pixels.append(env.physics.render(camera_id=camera_id, width=240))
+
+img = PIL.Image.fromarray(np.hstack(pixels))
+plt.imshow(img)
+plt.show()
