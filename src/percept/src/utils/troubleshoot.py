2ebefa412349f03f9d5e00b7ea76d311f285d214
tanmayyb <tbishnoi@torontomu.ca>
2025-01-01 22:40:06 +0530

    add debug function, fix stack depth

--- src/percept/src/utils/troubleshoot.py
+++ src/percept/src/utils/troubleshoot.py
@@ -4,11 +4,15 @@ import traceback
 # helper
 def get_current_function_name():
     stack = traceback.extract_stack()
-    return stack[-2].name 
+    return stack[-3].name 
 
 
-def get_error_text(e, show_funcname=True, show_error=True):
+def get_error_text(e, show_funcname=True, show_error=True, print_stack_trace=True):
     msg = ""
-    if show_funcname:  msg+= f"error in function: {get_current_function_name}"
-    if show_error:  msg+= f"{e}"
+    if show_funcname:  
+        msg+= f"error in function: '{get_current_function_name()}':"
+    if show_error:  
+        msg+= f" {e}"
+    if print_stack_trace:
+        msg+= f" {traceback.format_exc()}"
     return msg
