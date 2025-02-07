544cf22fc7ecb5fb766481648a4d087e121e15d5
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2025-02-07 13:18:56 -0500

    remove old structure

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-source /opt/ros/humble/setup.bash
-conda activate percept_env
-# source install/setup.bash
-colcon clean workspace -y && colcon build --symlink-install
-source install/setup.bash
