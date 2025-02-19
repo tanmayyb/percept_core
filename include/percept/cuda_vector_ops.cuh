473ac83f64cca75dff3da8b22d611f6449e5a7f4
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2025-02-19 15:37:11 -0500

    add random_vector generation

--- include/percept/cuda_vector_ops.cuh
+++ include/percept/cuda_vector_ops.cuh
@@ -3,7 +3,9 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <vector_types.h>
-
+#include <curand.h>
+#include <curand_kernel.h>
+#include <random>
 
 namespace cuda_vector_ops{
 
@@ -58,6 +60,27 @@ __host__ __device__ inline double3 normalized(const double3 &v) {
     } else {
         return make_double3(0.0, 0.0, 0.0);
     }
+}
+
+__host__ __device__ inline double3 make_random_vector() {
+    double3 ret;
+    #ifdef __CUDA_ARCH__
+        // GPU path using curand
+        curandState state;
+        curand_init(clock64(), threadIdx.x, 0, &state);
+        ret.x = 2.0 * curand_uniform_double(&state) - 1.0; // Scale to [-1,1]
+        ret.y = 2.0 * curand_uniform_double(&state) - 1.0;
+        ret.z = 2.0 * curand_uniform_double(&state) - 1.0;
+    #else
+        // CPU path using std random
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<double> dis(-1.0, 1.0);
+        ret.x = dis(gen);
+        ret.y = dis(gen);
+        ret.z = dis(gen);
+    #endif
+    return ret;
 }
 
 }
