602dbc03335824e998687f80476ed31fd186af13
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2025-02-24 04:41:13 -0500

    adds cams to maniskill

--- launch/sim_static.py
+++ launch/sim_static.py
@@ -19,84 +19,23 @@ def yaml_to_dict(path_to_yaml):
 def get_path(pkg_share:str, *paths):
     return os.path.join(pkg_share, *paths)
 
-# def create_perception_group(pkg_share:str):
-#     real_pipeline_params = {
-#         'static_camera_config': get_path(pkg_share, 'config', 'sim_static_cams.yaml'),
-#         'sim_pipeline_config': get_path(pkg_share, 'config', 'sim_pipeline_conf.yaml'), 
-#         'agent_config': get_path(pkg_share, 'config', 'agent_conf.yaml'),
-#         'static_scene': True,
-#         'static_agent': True
-#     }
+
+def create_sim_streamer_group(pkg_share:str, debug_mode, disable_render):
     
-#     static_tf_publisher_params = {
-#         'static_camera_config': get_path(pkg_share, 'config', 'sim_static_cams.yaml'),
-#         'agent_config': get_path(pkg_share, 'config', 'agent_conf.yaml')
-#     }
-
-#     namespace = 'perception'
-#     print(pkg_share)
-#     # Define the perception group
-#     perception_group = [
-#             Node(
-#                 package='percept',
-#                 executable='sim_pipeline.py',
-#                 name='sim_perception_node',
-#                 output='screen',
-#                 parameters=[sim_pipeline_params], # load all configs
-#                 namespace=namespace
-#             ),
-#             Node(
-#                 package='percept',
-#                 executable='static_tf_publisher.py',
-#                 name='static_tf_publisher',
-#                 parameters=[static_tf_publisher_params],
-#                 output='screen',
-#                 namespace=namespace  
-#             ),
-#             Node(
-#                 package='rviz2',
-#                 executable='rviz2',
-#                 name='perception_rviz',
-#                 arguments=['-d', get_path(pkg_share, 'config', 'perception.rviz')],
-#                 namespace='perception' 
-#             )
-#         ]
-
-#     return GroupAction(perception_group)
-
-
-# def create_realsense_group(pkg_share:str):
-#     realsense_group = []
-#     static_cams = yaml_to_dict(get_path(pkg_share, 'config', 'rs_static_cams.yaml'))
-#     global_params = yaml_to_dict(get_path(pkg_share, 'config', 'rs_cameras.yaml'))
-#     namespace = 'cameras'
-#     for camera_id, camera_info in static_cams.items():  
-#         params = deepcopy(global_params)
-#         # params['camera_name'] = camera_id
-#         # params['camera_namespace'] = namespace
-#         params['serial_no'] = camera_info['serial_no']
-#         node = Node(
-#             package='realsense2_camera',
-#             namespace=namespace,
-#             name=camera_id,
-#             executable='realsense2_camera_node',
-#             parameters=[params],
-#             # output=_output,
-#             # arguments=['--ros-args', '--log-level', 'debug'],
-#             # emulate_tty=True,
-#         )
-#         realsense_group.append(node)
-#     return GroupAction(realsense_group)
-
-
-def create_sim_streamer_group(pkg_share:str):
+    
+    sim_streamer_params = {
+        'sim_config': get_path(pkg_share, 'config', 'sim_conf.yaml'),
+        'env_id': 'SimEnv-v1',
+        'debug_mode': debug_mode,
+        'disable_render': disable_render
+    }
     sim_streamer_group = [
         Node(
             package='percept',
             executable='sim_stream.py',
             name='sim_streamer',
             output='screen',
-            # parameters=[sim_streamer_params],
+            parameters=[sim_streamer_params],
             namespace='sim_streamer'
         )
     ]
@@ -106,15 +45,28 @@ def create_sim_streamer_group(pkg_share:str):
 
 def generate_launch_description():
     pkg_share = get_package_share_directory('percept')
-    # perception_group = create_perception_group(pkg_share)
-    # realsense_group = create_realsense_group(pkg_share)
-
-    sim_streamer_group = create_sim_streamer_group(pkg_share)
-
     
+    # Declare the debug_mode launch argument
+    debug_mode_arg = DeclareLaunchArgument(
+        'debug_mode',
+        default_value='false',
+        description='Enable debug mode'
+    )
+
+    disable_render_arg = DeclareLaunchArgument(
+        'disable_render',
+        default_value='false',
+        description='Disable rendering'
+    )
+    
+    sim_streamer_group = create_sim_streamer_group(
+        pkg_share,
+        LaunchConfiguration('debug_mode'),
+        LaunchConfiguration('disable_render')
+    )
+
     # Combine all nodes into the launch description
     return LaunchDescription([
-        # perception_group,
-        # realsense_group,
+        debug_mode_arg,
         sim_streamer_group
     ])
